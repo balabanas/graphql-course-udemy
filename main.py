66ae8804b3dcf8af77c03e81f5984ef607c66f0c
Anton Balabanov <asbbnv@gmail.com>
2023-11-28 10:23:36 +0200

    add apps listing

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from graphene import Schema
 from starlette_graphene3 import GraphQLApp, make_playground_handler
 
-from app.db.database import prepare_database
+from app.db.database import prepare_database, Session
+from app.db.models import JobApplication
 from app.gql.mutation import Mutation
 from app.gql.queries import Query
 
@@ -16,7 +17,12 @@ def startup_event():
     prepare_database()
 
 
-app.mount("/", GraphQLApp(
+@app.get("/apps")
+def get_applications():
+    with Session() as session:
+        return session.query(JobApplication).all()
+
+app.mount("/gql", GraphQLApp(
     schema=schema,
     on_get=make_playground_handler())
           )
